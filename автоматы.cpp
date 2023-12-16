22cb1d8534124ca39ea650941cdd8e3e3a74c520
Гена <@GOLOGEN>
2023-12-16 17:40:14 +0300

    Добавьте файлы проекта.

---
+++ автоматы.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string fname = "INPUT.TXT";
+	string oname = "OUTPUT.TXT";
+	int d, N, n, m;
+	ifstream fileInput(fname);
+	ofstream fileOutput(oname);
+	if (fileInput.is_open())
+	{
+		cout << "Файл открыт" << endl;
+		fileInput >> N;
+
+		for (int i = 0; i < N;i++)
+		{
+			fileInput >> n;
+			fileInput >> m;
+			d = 19 * m + (n + 239) * (n + 366) / 2;
+			fileOutput << d << endl;
+		}
+		
+		fileOutput.close();
+		
+		//return 0;
+	}
+}
